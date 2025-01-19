de5f6a8cef7779103f8d297528ef4e9abff23198
David Russell <druss337@gmail.com>
2025-01-19 13:02:57 -0700

    prod url

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
       ignoreDuringBuilds: true,
     },
     env: {
-        AUTH0_BASE_URL: process.env.VERCEL_URL || process.env.AUTH0_BASE_URL,
+        AUTH0_BASE_URL:  process.env.AUTH0_BASE_URL  || process.env.VERCEL_URL,
         AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
         AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
         AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
