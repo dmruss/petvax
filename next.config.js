acbf835c3737445df3c12785c743ccd3d8626b16
David Russell <druss337@gmail.com>
2025-01-18 20:33:05 -0700

    next env export

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
     eslint: {
       ignoreDuringBuilds: true,
     },
+    env: {
+        AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
+        AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
+        AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+        AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+      },
   };
   
   module.exports = nextConfig;
